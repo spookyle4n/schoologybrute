91087fa96b6c798965bcf26873c91ad8a47ef8e8
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-11-04 14:45:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -41,5 +41,5 @@ if a == 'y':
         if randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter == '676848Vr':
             print("\033[32m| attempting... \033[1;3m676848Vr\033[0m |\n\tsuccess! [\u2713] ")
             
-        print("\033[31m| attempting..." + randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter + " |", flush=True)
+        print("\033[31m| attempting... " + randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter + " |", flush=True)
         time.sleep(0.0000001)
